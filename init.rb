aa05032a0d6c83e8d8f5281b17536c4c2d434b1a
wenke <liangwenke.com@gmail.com>
2013-03-30 20:59:05 +0800

    Fixed commas and " are not escaped

--- init.rb
+++ init.rb
@@ -1,1 +1,2 @@
+# -*- encoding : utf-8 -*-
 require 'to_csv-rails'
--- lib/to_csv-rails.rb
+++ lib/to_csv-rails.rb
@@ -1,3 +1,4 @@
+# -*- encoding : utf-8 -*-
 class Array
 
   def to_csv(options = {})
@@ -22,7 +23,7 @@ class Array
     end
 
     self.each do |obj|
-      data << columns.map{ |column| obj.send(column) }.join(',')
+      data << columns.map { |column| "\'#{column_string.to_s}\'" rescue '' }.join(',')
     end
     data.join("\n")
   end
